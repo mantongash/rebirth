389d4317dc9387f38479d094af62eed066cb714c
Sam Okello <samokello024@gmail.com>
2025-11-19 08:28:46 +0300

    Updated assets to asset

--- client/src/pages/AdminProfile.js
+++ client/src/pages/AdminProfile.js
@@ -4,7 +4,7 @@ import { FaSave, FaCheckCircle, FaSpinner, FaExclamationCircle } from 'react-ico
 import { useAdminAuth } from '../context/AdminAuthContext';
 import { useNotification } from '../context/NotificationContext';
 import { getBaseUrl } from '../utils/apiConfig';
-import logo from "../assets/logo-1.png";
+import logo from "../asset/logo-1.png";
 
 const Container = styled.div`
   min-height: 100vh;
